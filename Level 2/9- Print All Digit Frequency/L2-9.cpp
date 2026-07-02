fc724124f1603537a1fb0a00d9d4c381f9c31b28
shadi zainaldeen <shadi.zain.aldeen2004@gmail.com>
2026-07-02 11:57:19 +0300

    Enhance Problem 9 Of Level 2

--- Level 2/9- Print All Digit Frequency/L2-9.cpp
+++ Level 2/9- Print All Digit Frequency/L2-9.cpp
@@ -3,36 +3,27 @@
 #include "../../myLib.h"
 using namespace std;
 
-void calcDigitsFrequency(int number, int freq[10], bool flags[10])
+void calcAllDigitsFrequency(int number, int freq[10])
 {
-    int digit, number2 = number;
+    int digit;
 
-    while (number2 > 0)
+    while (number > 0)
     {
-        digit = number2 % 10;
-
-        if (!flags[digit])
-        {
-            freq[digit] = calcDigitFrequency(number, digit);
-            flags[digit] = true;
-        }
-        number2 /= 10;
+        digit = number % 10;
+        freq[digit]++;
+        number /= 10;
     }
 }
 
 void printAllDigitsFrequency(int number)
 {
-    int freq[10];
-    bool flags[10];
+    int freq[10] = {0};
 
-    fill(begin(flags), end(flags), 0);
-    fill(begin(freq), end(freq), 0);
-
-    calcDigitsFrequency(number, freq, flags);
+    calcAllDigitsFrequency(abs(number), freq);
 
     for (int i = 0; i < 10; i++)
     {
-        if (flags[i])
+        if (freq[i] > 0)
             cout << "Digit " << i << " Frequency is " << freq[i] << " Time(s)." << endl;
     }
 }
